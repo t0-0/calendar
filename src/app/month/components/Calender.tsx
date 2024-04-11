4bbd385dd0452942c00d619d0eb5fc05f14b4f80
t0-0 <107523324+t0-0@users.noreply.github.com>
2024-04-11 19:58:41 +0900

    add calendar

--- src/app/month/components/Calender.tsx
+++ src/app/month/components/Calender.tsx
@@ -1,5 +1,33 @@
+"use client";
+
+import { useState } from "react";
+
 const Calender = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const dayOfWeek = ["月", "火", "水", "木", "金", "土", "日"];
+  const firstDayOfMonth = () => {
+    const tmpDate = new Date();
+    tmpDate.setFullYear(selectedDate.getFullYear(), selectedDate.getMonth(), 1);
+    return (tmpDate.getDay() + 6) % 7;
+  };
+  const lastDate = () => {
+    const tmpDate = new Date();
+    tmpDate.setFullYear(
+      selectedDate.getFullYear(),
+      selectedDate.getMonth() + 1,
+      0
+    );
+    return tmpDate.getDate();
+  };
+  const lastDayOfMonth = () => {
+    const tmpDate = new Date();
+    tmpDate.setFullYear(
+      selectedDate.getFullYear(),
+      selectedDate.getMonth() + 1,
+      0
+    );
+    return (tmpDate.getDay() + 6) % 7;
+  };
   return (
     <div className="flex flex-col items-center">
       20xx年y月
@@ -12,6 +40,19 @@ const Calender = () => {
             {day}
           </div>
         ))}
+        {Array.from({ length: firstDayOfMonth() }).map((_, i) => {
+          return <div key={i} className="border"></div>;
+        })}
+        {Array.from({ length: lastDate() }).map((_, i) => {
+          return (
+            <div key={i} className="border flex items-center text-center justify-center aspect-square">
+              {i}
+            </div>
+          );
+        })}
+        {Array.from({ length: 6 - lastDayOfMonth() }).map((_, i) => {
+          return <div key={i} className="border"></div>;
+        })}
       </div>
     </div>
   );
